3c432dd4b00a0e532a1d82037a150bdcc1f293e9
ch0p1k3 <ch00p.228@gmail.com>
2023-03-21 23:43:59 +0300

    right template

---
+++ lib/src/main/kotlin/com/github/decentralized_hse/messenger_http_transport/api2ch/Captcha.kt
@@ -0,0 +1,8 @@
+package com.github.decentralized_hse.messenger_http_transport.api2ch
+
+data class Captcha(val captchaType: String, val captchaId: Long, val captchaValue: String) {
+}
+
+class CaptchaHelper {
+
+}
---
+++ lib/src/main/kotlin/com/github/decentralized_hse/messenger_http_transport/transport/Library.kt
@@ -0,0 +1,8 @@
+package com.github.decentralized_hse.messenger_http_transport.transport
+
+class Library {
+}
+
+class Library2 {
+
+}
